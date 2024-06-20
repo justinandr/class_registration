8bf34ac1bb7b1a403ad092c7c87552bf7c5db840
Justin Rodriguez <justin.r@me.com>
2024-06-20 17:52:46 -0400

    create students by id view

--- server/app.py
+++ server/app.py
@@ -23,22 +23,52 @@ class Students(Resource):
     def post(self):
         data = request.get_json()
 
-        try:
-            new_student = Student(
-                name = data['name'],
-                year = data['year'],
-                major = data['major']
-            )
+        new_student = Student(
+            name = data['name'],
+            year = data['year'],
+            major = data['major']
+        )
 
-            db.session.add(new_student)
+        db.session.add(new_student)
+        db.session.commit()
+
+        return make_response(new_student.to_dict(), 201)
+        
+class StudentsById(Resource):
+    def get(self, id):
+        student = Student.query.filter_by(id = id).first()
+        
+        if student:
+            return student.to_dict(), 200
+        return {"error": "404 Not Found"}, 404
+    
+    def patch(self, id):
+        student = Student.query.filter_by(id = id).first()
+        data = request.get_json()
+
+        if student:
+            for attr in data:
+                setattr(student, attr, data[attr])
+
+            db.session.add(student)
             db.session.commit()
 
-            return make_response(new_student.to_dict(), 200)
-        except ValueError as ve:
-            return {"error": f"{ve}"}, 400
+            return make_response(student.to_dict(), 200)
+        
+        return {"error": "404 Not Found"}, 404
+    
+    def delete(self, id):
+        student = Student.query.filter_by(id = id).first()
+
+        if student:
+            db.session.delete(student)
+            db.session.commit()
+        
+        return {"error": "404 Not Found"}, 404
 
 api.add_resource(Home, '/')
 api.add_resource(Students, '/students')
+api.add_resource(StudentsById, '/students/<int:id>')
 
 
 if __name__ == '__main__':
