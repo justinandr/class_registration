73438494e527d9127778080ed7ea962dbbe26580
Justin Rodriguez <justin.r@me.com>
2024-06-19 22:12:41 -0400

    add models / seed data

--- server/models.py
+++ server/models.py
@@ -1,6 +1,37 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from datetime import datetime
 
 from config import db
 
-# Models go here!
+class Student(db.Model, SerializerMixin):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    year = db.Column(db.Integer, nullable = False)
+    major = db.Column(db.String)
+
+    def __repr__(self):
+        return f'Name: {self.name}, Year: {self.year}, Major: {self.major}'
+    
+class Class(db.Model, SerializerMixin):
+    __tablename__ = 'classes'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    location = db.Column(db.String, nullable = False)
+    days = db.Column(db.String, nullable = False)
+    start_time = db.Column(db.Time, nullable = False)
+    end_time = db.Column(db.Time, nullable = False)
+
+    def __repr__(self):
+        return f'Class: {self.name}, Location: {self.location}, Days: {self.days}, Time: {self.start_time} - {self.end_time}'
+    
+class Registration(db.Model, SerializerMixin):
+    __tablename__ = 'regristrations'
+
+    id = db.Column(db.Integer, primary_key = True)
+    term = db.Column(db.String, nullable = False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable = False)
+    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), nullable = False)
--- server/seed.py
+++ server/seed.py
@@ -1,17 +1,70 @@
 #!/usr/bin/env python3
 
-# Standard library imports
 from random import randint, choice as rc
 
-# Remote library imports
 from faker import Faker
 
-# Local imports
 from app import app
-from models import db
+from models import db, Student, Class, Registration
+
+fake = Faker()
+
+days = ['Monday, Wednesday, Friday', 'Tuesday, Thursday']
+terms = ['Fall', 'Spring']
+
+def create_students():
+    students = []
+
+    for s in range(20):
+        student = Student(fake.name(), randint(1, 4), fake.job())
+        students.append(student)
+    
+    return students
+
+def create_classes():
+    classes = []
+
+    for c in range(20):
+        cl = Class(
+            fake.job(), 
+            fake.street_address(),
+            rc(days),
+            fake.time(),
+            fake.time() 
+            )
+        classes.append(cl)
+    return classes
+
+def create_registrations():
+    registrations = []
+
+    for r in range(20):
+        registration = Registration(rc(terms), r + 1, randint(1, 20))
+        registrations.append(registration)
+
+    return registrations
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+        print("Clearing db...")
+        Student.query.delete()
+        Class.query.delte()
+        Registration.query.delete()
+
+        print('Seeding students...')
+        students = create_students()
+        db.session.add_all(students)
+        db.session.commit()
+
+        print('Seeding classes...')
+        cl = create_classes()
+        db.session.add_all(cl)
+        db.session.commit()
+        
+        print('Seeding registrations...')
+        registrations = create_registrations()
+        db.session.add_all(registrations)
+        db.session.commit()
+
+        print("DB seeded successfully.")
