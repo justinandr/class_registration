65fc8a4316f9fc266c607ad1c6c745375c24d5e1
Justin Rodriguez <justin.r@me.com>
2024-06-21 06:21:53 -0400

    test app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+
+    const [students, setStudents] = useState([])
+
+    useEffect(() => {
+        fetch('/students')
+        .then(res => res.json())
+        .then((data) => setStudents(data))
+    }, [])
+
+    students.forEach(student => console.log(student.name))
 }
 
 export default App;
--- server/models.py
+++ server/models.py
@@ -1,5 +1,3 @@
-#HIT ALL CONSTRAINT REQUIREMENTS
-
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from datetime import datetime
